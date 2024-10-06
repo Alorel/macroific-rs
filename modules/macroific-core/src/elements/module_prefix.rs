00546d83e8522ca59d170b699d4fa227ee54cc8c
Art <4998038+Alorel@users.noreply.github.com>
2024-10-06 13:45:29 +0100

    feat(core): Add `ModulePrefix.chain` method as an analog to removed suffix functionality

--- modules/macroific-core/src/elements/module_prefix.rs
+++ modules/macroific-core/src/elements/module_prefix.rs
@@ -21,25 +21,30 @@ pub const RESULT: ModulePrefix<'static, 3> = ModulePrefix::new(["core", "result"
 /// # use syn::parse_quote;
 /// # use quote::{quote, ToTokens};
 /// #
-/// const PREFIXED: ModulePrefix<'static, 2> = ModulePrefix::new(["foo", "bar"]);
-/// const UNPREFIXED: ModulePrefix<'static, 2> = ModulePrefix::new(["foo", "bar"])
-///   .with_leading_sep(false);
+/// let prefixed = ModulePrefix::new(["foo", "bar"]);
+/// let prefixed_stream = prefixed.to_token_stream().to_string();
+/// assert_eq!(prefixed_stream, ":: foo :: bar");
 ///
-/// let tokens_prefixed = PREFIXED.to_token_stream().to_string();
-/// let tokens_unprefixed = UNPREFIXED.to_token_stream().to_string();
-///
-/// assert_eq!(tokens_prefixed, ":: foo :: bar");
-/// assert_eq!(tokens_unprefixed, "foo :: bar");
+/// let unprefixed = prefixed.with_leading_sep(false);
+/// let unprefixed_stream = unprefixed.to_token_stream().to_string();
+/// assert_eq!(unprefixed_stream, "foo :: bar");
 ///
 /// // Display is also implemented
-/// assert_eq!(PREFIXED.to_string(), tokens_prefixed);
-/// assert_eq!(UNPREFIXED.to_string(), tokens_unprefixed);
+/// assert_eq!(prefixed.to_string(), prefixed_stream);
+/// assert_eq!(unprefixed.to_string(), unprefixed_stream);
 /// ```
 #[derive(Copy, Clone, Eq, PartialEq, Hash, Ord, PartialOrd, Debug)]
 #[cfg(feature = "module-prefix")]
 pub struct ModulePrefix<'a, const LEN: usize> {
     path: [&'a str; LEN],
     leading_sep: bool,
+}
+
+/// A chained [`ModulePrefix`] separated by `::`.
+#[cfg(feature = "module-prefix")]
+pub struct Chain<A, B> {
+    a: A,
+    b: B,
 }
 
 impl<'a, const LEN: usize> ModulePrefix<'a, LEN> {
@@ -150,5 +155,102 @@ impl<'a, const LEN: usize> Index<usize> for ModulePrefix<'a, LEN> {
     #[inline]
     fn index(&self, index: usize) -> &Self::Output {
         self.path[index]
+    }
+}
+
+impl<const LEN: usize> ModulePrefix<'_, LEN> {
+    /// Chain the path with another segment - typically an [`Ident`], [`Path`](syn::Path), or
+    /// another [`ModulePrefix`] or [`Chain`].
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use macroific_core as macroific;
+    /// # use macroific::core_ext::*;
+    /// # use macroific::elements::ModulePrefix;
+    /// # use syn::{DeriveInput, parse_quote};
+    /// # use proc_macro2::{Ident, TokenStream};
+    /// # use quote::quote;
+    /// #
+    /// const MAIN_MODULE: ModulePrefix<'static, 2> = ModulePrefix::new(["my_crate", "some_module"]);
+    ///
+    /// // Simplified view of a macro
+    /// fn parse() -> DeriveInput {
+    ///   parse_quote!(struct Foo(u8);) // would be an actual token stream in a derive macro
+    /// }
+    ///
+    /// # //noinspection RsConstantConditionIf
+    /// fn to_tokens(input: DeriveInput) -> TokenStream {
+    ///   const SOME_CONDITION: bool = true;
+    ///   const ANOTHER_CONDITION: bool = false;
+    ///
+    ///   let submodule = MAIN_MODULE.chain(Ident::create(if SOME_CONDITION {
+    ///     "foo"
+    ///   } else {
+    ///     "bar"
+    ///   }));
+    ///
+    ///   let trait_name = submodule.chain(Ident::create(if ANOTHER_CONDITION {
+    ///     "Qux"
+    ///   } else {
+    ///     "Baz"
+    ///   }));
+    ///
+    ///   // Implement some imaginary trait forwarding
+    ///   let ident = &input.ident;
+    ///   quote! {
+    ///     impl #trait_name for #ident {
+    ///       fn exec(self) {
+    ///         #trait_name::exec(self.inner);
+    ///       }
+    ///     }
+    ///   }
+    /// }
+    ///
+    /// let input = parse();
+    /// let output = to_tokens(input);
+    ///
+    /// assert_eq!(output.to_string(), "impl :: my_crate :: some_module :: foo :: Baz for Foo { fn exec (self) { :: my_crate :: some_module :: foo :: Baz :: exec (self . inner) ; } }");
+    /// ```
+    ///
+    /// Output in a bit more readable format:
+    ///
+    #[cfg_attr(doctest, doc = " ````no_test")]
+    /// ```
+    /// impl ::my_crate::some_module::foo::Baz for Foo {
+    ///     fn exec(self) {
+    ///         ::my_crate::some_module::foo::Baz::exec(self.inner);
+    ///     }
+    /// }
+    /// ````
+    pub const fn chain<T>(self, next_segment: T) -> Chain<Self, T>
+    where
+        T: ToTokens,
+    {
+        Chain {
+            a: self,
+            b: next_segment,
+        }
+    }
+}
+
+impl<A, B> Chain<A, B> {
+    /// Chain the path further. See [`ModulePrefix::chain`].
+    pub const fn chain<C>(self, next_segment: C) -> Chain<Self, C>
+    where
+        C: ToTokens,
+    {
+        Chain {
+            a: self,
+            b: next_segment,
+        }
+    }
+}
+
+impl<A: ToTokens, B: ToTokens> ToTokens for Chain<A, B> {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        self.a.to_tokens(tokens);
+        <Token![::]>::default().to_tokens(tokens);
+        self.b.to_tokens(tokens);
     }
 }
